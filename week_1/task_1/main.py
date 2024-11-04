7f6511edac5bfb6c9b2fbdb72f1900c9cfb79529
Memoey <git@memoey.icu>
2024-11-05 00:38:50 +0100

    [task-1] style: rename variables

--- week_1/task_1/main.py
+++ week_1/task_1/main.py
@@ -16,6 +16,7 @@ USERNAME = os.environ["TASK1_USERNAME"]
 PASSWORD = os.environ["TASK1_PASSWORD"]
 TIMEOUT_DEFAULT = 60
 
+
 def get_current_question() -> str:
     """Get current login question"""
     response = requests.get(WEBSITE_URL, timeout=TIMEOUT_DEFAULT)
@@ -24,11 +25,14 @@ def get_current_question() -> str:
     soup = BeautifulSoup(response.text, "html.parser")
     question_paragraph = soup.find("p", id="human-question")
     if question_paragraph:
-        question = question_paragraph.get_text().strip().replace("Question:", "").strip()
+        question = (
+            question_paragraph.get_text().strip().replace("Question:", "").strip()
+        )
         LOG.info("Retrive question: %s", question)
         return question
     LOG.error("No question was found in <p> with id='human-question' on the website.")
     return None
+
 
 def get_llm_answer(question: str) -> None:
     """Given question returns a valid answer"""
@@ -49,23 +53,26 @@ def get_llm_answer(question: str) -> None:
     LOG.info("The answer is: %s", answer)
     return answer
 
+
 def login_with_answer(answer: str) -> str:
     """Given the answer for question send POST request to login"""
     form_data = {"username": USERNAME, "password": PASSWORD, "answer": answer}
     LOG.debug("Send following request to log in: %s", form_data)
-    login_response = requests.post(WEBSITE_URL, data=form_data, timeout=TIMEOUT_DEFAULT)
-    if login_response.status_code != HTTPStatus.OK:
-        LOG.error("Login failed, status code = %s", login_response.status_code)
-    return login_response.text
+    response = requests.post(WEBSITE_URL, data=form_data, timeout=TIMEOUT_DEFAULT)
+    if response.status_code != HTTPStatus.OK:
+        LOG.error("Login failed, status code = %s", response.status_code)
+        return None
+    return response.text
 
-def process_response(login_response: str) -> str:
+
+def process_response(response: str) -> str:
     """Extract flags and secret link from login response"""
     flag_pattern = re.compile(r"{{FLG:[a-zA-Z0-9]+}}")
-    flags = flag_pattern.findall(login_response)
+    flags = flag_pattern.findall(response)
     if flags:
         LOG.info("Flags found: %s", " ".join(flags))
-    soup = BeautifulSoup(login_response, "html.parser")
-    LOG.debug("The following response was sent after login: %s", login_response)
+    soup = BeautifulSoup(response, "html.parser")
+    LOG.debug("The following response was sent after login: %s", response)
     secret_link = soup.find("a", href=True)
     if secret_link:
         LOG.info("Secret link is: %s", secret_link["href"])
@@ -73,8 +80,9 @@ def process_response(login_response: str) -> str:
     LOG.error("No secret link found")
     return None
 
+
 if __name__ == "__main__":
     current_question = get_current_question()
     current_answer = get_llm_answer(current_question)
-    response = login_with_answer(current_answer)
-    link, flags = process_response(response)
+    login_response = login_with_answer(current_answer)
+    process_response(login_response)
