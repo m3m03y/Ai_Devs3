9807c0b32c635fc06e9d22e06beeee01298fc6e9
Memoey <git@memoey.icu>
2024-11-14 22:48:42 +0100

    conf: set logging to file

--- api/conf/logger.py
+++ api/conf/logger.py
@@ -1,7 +1,17 @@
 """Common logger"""
 
 import logging
+import os
+import datetime
 
-logging.basicConfig(level=logging.INFO)
+PROJECT_DIR = os.environ["PROJECT_DIR"]
+
+log_dir = os.fspath(f"{PROJECT_DIR}/logs")
+
+if not os.path.isdir(log_dir):
+    os.mkdir(log_dir)
+
+logfile = os.fspath(f"{log_dir}/{datetime.datetime.now()}.log")
+logging.basicConfig(level=logging.INFO, filename=logfile)
 
 LOG = logging.getLogger("main")
