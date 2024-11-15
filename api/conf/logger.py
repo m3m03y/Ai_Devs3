1c753cf591a76c36da1c6bf2a70d4878b44aeb8e
Memoey <git@memoey.icu>
2024-11-15 17:10:25 +0100

    conf: add handlers to log to file and console

--- api/conf/logger.py
+++ api/conf/logger.py
@@ -12,6 +12,20 @@ if not os.path.isdir(log_dir):
     os.mkdir(log_dir)
 
 logfile = os.fspath(f"{log_dir}/{datetime.datetime.now()}.log")
-logging.basicConfig(level=logging.INFO, filename=logfile)
 
 LOG = logging.getLogger("main")
+
+LOG.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(logfile)
+console_handler = logging.StreamHandler()
+
+file_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+LOG.addHandler(file_handler)
+LOG.addHandler(console_handler)
